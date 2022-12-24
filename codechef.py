2015cf1ac5c62fa4d7a122a17d0ea083e17f7333
sona@1027 <118789307+sowmyasona@users.noreply.github.com>
2022-12-24 22:41:35 +0530

    Add files via upload

---
+++ codechef.py
@@ -0,0 +1,31 @@
+# Adding of two numbers
+
+# T = int(input())
+# for tc in range(T):
+# 	# Read integers a and b.
+# 	(a, b) = map(int, input().split(' '))
+	
+# 	ans = a + b
+# 	print(ans)
+
+# REVERSE THE NUMBER
+
+# t = int(input())
+# for i in range(t):
+#     reverse = 0
+#     num = int(input())
+#     while(num):
+#         remainder = num%10
+#         reverse =reverse*10+remainder
+#         num = int(num/10)
+#     print(reverse)
+
+# SUM OF DIGITS
+
+# T=int(input())
+# for i in range(T):
+#     num=int(input())
+#     sum=0
+#     for j in str(num):
+#         sum+=int(j)
+#     print(sum)
